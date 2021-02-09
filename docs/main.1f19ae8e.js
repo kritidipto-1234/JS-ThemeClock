d2ef08815b4c71eb5b740b8773b7c7bf8ba0e236
Kritidipto Ghosh <kritidiptodgp.ghosh@gmail.com>
2021-02-10 01:16:16 +0530

    some changes

--- docs/main.1f19ae8e.js
+++ docs/main.1f19ae8e.js
@@ -506,7 +506,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42029" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43237" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -683,4 +683,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel/src/builtins/hmr-runtime.js","main.js"], null)
-//# sourceMappingURL=main.1f19ae8e.js.map
+//# sourceMappingURL=/main.1f19ae8e.js.map
